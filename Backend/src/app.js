bff6908078797bbeeb4a46bbe49dfe0fcce7f676
Enos-AL <ciscopackettracent@gmail.com>
2024-08-26 16:57:05 -0300

    inserindo dados no SQL Server

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -34,6 +34,55 @@ connectToDatabase();
 // Middleware para analisar JSON
 app.use(express.json());
 
+// Função para verificar se a coluna existe
+async function checkAndAddColumn() {
+    try {
+      await connectToDatabase();
+      
+      // Verifica se a coluna "Senha" existe
+      const result = await sql.query`
+        SELECT COLUMN_NAME 
+        FROM INFORMATION_SCHEMA.COLUMNS 
+        WHERE TABLE_NAME = 'Usuarios' 
+        AND COLUMN_NAME = 'Senha'
+      `;
+      
+      if (result.recordset.length === 0) {
+        // Se a coluna "Senha" não existe, adicione-a
+        await sql.query`
+          ALTER TABLE Usuarios
+          ADD Senha NVARCHAR(255);
+        `;
+        console.log('Coluna "Senha" foi adicionada à tabela Usuario.');
+      } else {
+        console.log('Coluna "Senha" já existe na tabela Usuario.');
+      }
+    } catch (err) {
+      console.error('Erro ao verificar ou adicionar coluna:', err);
+    }
+  }
+
+// Rota para inserir um usuário no banco de dados
+app.post('/usuarios', async (req, res) => {
+    const { nome, email, senha } = req.body;
+  
+    try {
+      // Verificar e adicionar a coluna "Senha" se necessário
+      await checkAndAddColumn();
+  
+      // Inserir dados na tabela Usuario
+      const result = await sql.query`
+        INSERT INTO Usuarios (Nome, Email, Senha)
+        VALUES (${nome}, ${email}, ${senha})
+      `;
+  
+      res.status(201).send('Usuário inserido com sucesso!');
+    } catch (err) {
+      console.error('Erro ao inserir dados:', err);
+      res.status(500).send('Erro ao inserir dados no banco de dados.');
+    }
+  });
+
 // Rota básica para testar a comunicação com o banco de dados
 app.get('/usuarios', async (req, res) => {
     try {
