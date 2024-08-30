7fba73767bae0107470dc1d74aec16f9319750a0
Enos-AL <ciscopackettracent@gmail.com>
2024-08-30 16:30:16 -0300

    Tornando código do arquivo LocalizarUsuarioPage.js Front-End limpo, comentário continua para compreensão de estudos.

--- frontend/src/pages/LocalizarUsuarioPage.js
+++ frontend/src/pages/LocalizarUsuarioPage.js
@@ -14,7 +14,7 @@ const LocalizarUsuarioPage = () => {
             if (dados.message) {
                 // Quando o backend retorna uma mensagem, exiba-a como um alerta
                 alert(dados.message);
-                setUsuario(null); // Limpa os dados do usuário caso haja uma mensagem
+                setUsuario(null);
             } else {
                 // Quando o backend retorna os dados do usuário completo
                 setUsuario(dados);
@@ -23,9 +23,6 @@ const LocalizarUsuarioPage = () => {
             // Suprime o erro no console e exibe apenas mensagens de erro específicas
             if (error.message.includes('Esse usuário não consta no Banco de Dados') ||
                 error.message.includes('Usuário encontrado, mas é necessário digitar o nome completo para localizar todos os dados.')) {
-                // // console.warn('Erro tratado: ', error.message);
-            } else {
-                console.error('Erro ao localizar usuário:', error);
             }
             alert('Erro ao localizar usuário. Tente novamente mais tarde.');
         }
@@ -34,8 +31,6 @@ const LocalizarUsuarioPage = () => {
     const handleAlterar = () => {
         if (usuario && usuario.id) {
             navigate(`/alterar-usuario/${usuario.id}`, { state: { usuario } });
-        } else {
-            console.error('Usuário não encontrado ou ID ausente.');
         }
     };
 
